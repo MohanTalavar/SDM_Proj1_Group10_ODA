f96bf2ad19ffd858258befa3607513cad65d523a
Shubham Ghodkhande <shubhamg0615@gmail.com>
2023-04-20 21:16:00 +0530

    modified ODA & added logic for password authentication

--- db_ODA.sql
+++ db_ODA.sql
@@ -1,15 +1,18 @@
 create table Patient (
  pid int primary key,
- pname varchar(20) not null,
- pmno int not null,
- pdesc varchar(80) 
+ pname varchar(30) not null,
+ pphno varchar(10) not null unique,
+ paddress varchar(100),
+ pmail varchar(100) not null unique,
+ ppass varchar (20) not null
  );
 	
 create table Doctor (
  did int primary key,
- dname varchar(20) not null,
- demail varchar(20)  not null,   
- specialization varchar(20) not null
+ dname varchar(30) not null,
+ dmail varchar(80)  not null,   
+ specialization varchar(50) not null,
+ dpass varchar(20) not null
  );
 	
 	
@@ -17,19 +20,19 @@ create table Appointment (
  Aptid int primary key,
  pid int,
  did int,
- atpdate date,
- atptime int,
+ testid int,
+ Adate date not null,
+ Atime int not null,
  constraint fkk_pid foreign key(pid) references Patient(pid),
- constraint fk_did foreign key(did) references Doctor(did)
+ constraint fk_did foreign key(did) references Doctor(did),
+ constraint fk_test foreign key(testid) references Doctor(testid)
  );
 	
 	
 	
 create table Test (
  testid int primary key,
- pid int,
- tname varchar(20) not null,
- constraint fk_pid foreign key(pid) references Patient(pid)
+ tname varchar(80) not null
  );
 	
 	
